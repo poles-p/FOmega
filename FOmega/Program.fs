f9e61cba4c83bf0ee3259415ee8684f68d4a893f
poles-p <poles_p@wp.pl>
2011-02-11 18:17:20 +0100

    [doc] Kilka własności (jeszcze bez dowodu). [code] Możliwość uruchomienia kodu z pliku (@<nazwa pliku>).

--- FOmega/Program.fs
+++ FOmega/Program.fs
@@ -5,17 +5,21 @@ open Sub
 let main =
     while true do
         System.Console.Write "> ";
-        match System.Console.ReadLine() |> Parser.parsujText with
-        | Parsor.Core.Success term ->
+        let ins = System.Console.ReadLine();
+        if ins.Length > 0 then
             try
-                opt{
-                    let! (s, typ) = AlgorytmW.rekTyp (KontekstTypowania[]) term;
-                    let! (_, kind) = AlgorytmW.rekRodzaj (KontekstTypowania[]) typ;
-                    System.Console.WriteLine("{0}  :  {1}  ::  {2}", s.Aplikuj term, typ, kind);
-                    return ()
-                } |> ignore
+                match (if ins.[0] = '@' then System.IO.File.ReadAllText(ins.Substring 1) else ins) |> Parser.parsujText with
+                | Parsor.Core.Success term ->
+                    opt{
+                        let! (s, typ) = AlgorytmW.rekTyp (KontekstTypowania[]) term;
+                        let! (_, kind) = AlgorytmW.rekRodzaj (KontekstTypowania[]) typ;
+                        System.Console.WriteLine("{0}  :  {1}  ::  {2}", s.Aplikuj term, typ, kind);
+                        return ()
+                    } |> ignore                
+                | Parsor.Core.Error -> ()
             with
             | SubstitutionException ex ->
                 System.Console.WriteLine("Error : {0}", ex)
-        | Parsor.Core.Error -> ()
+            | :? System.IO.IOException as ex ->
+                System.Console.WriteLine("IO Exception : {0}", ex.Message)
     done
